dcf3fd67f25d06c68235ab6e089b86b934ca049f
unknown <1ksy3629@naver.com>
2023-09-27 15:59:39 +0900

    0927

--- 99_algorithm/BOJ/9weeks/21327_quiz.py
+++ 99_algorithm/BOJ/9weeks/21327_quiz.py
@@ -1,31 +1,34 @@
+# INF = int(1e9)
 import sys
-
 input = sys.stdin.readline
 
-N, M, K, B = map(int, input().split())
+N, M, K, B = map(int, input().split())  # 문제수, 카테고리 수, 풀 수 있는 문제 수, 보너스 점수
 
-categories = [[] for _ in range(M)]
+categories = [list() for _ in range(M + 1)]
 
 for _ in range(N):
-    p, c = map(int, input().split())
-    categories[c - 1].append(p)
+    score, category = map(int, input().split())
+    categories[category].append(score)
 
-for i in range(M):
+for i in range(1, M + 1):
     categories[i].sort(reverse=True)
+    categories[i].insert(0, 0)
     for j in range(1, len(categories[i])):
-        categories[i][j] += categories[i][j - 1]
+        categories[i][j] = categories[i][j] + categories[i][j - 1]
+        if j == len(categories[i]) - 1:
+            categories[i][j] += B
 
+# print(categories)
 dp = [[0] * (K + 1) for _ in range(M + 1)]
 
-for m in range(1, M + 1):
-    for k in range(1, K + 1):
-        for i in range(len(categories[m - 1]) + 1):
-            if k - i < 0:
-                break
-
-            inc = 0 if i == 0 else categories[m - 1][i - 1]
-            if i == len(categories[m - 1]):
-                inc += B
-            dp[m][k] = max(dp[m][k], dp[m - 1][k - i] + inc)
+for i in range(1, M + 1):  # i번 카테고리에서 선택
+    for j in range(1, K + 1):  # K번 문제까지 선택할 수 있음
+        for k in range(min(j + 1, len(categories[i]))):  # 해당 카테고리의 문제수 or j중 낮은 값
+            temp_score = categories[i][k]
+            # print(temp_score, str(i) + '번 카테고리', str(k) + '번 문제', str(j) + '번 문제까지 최대값')
+            # print(dp[i - 1][j - k] + temp_score)
+            dp[i][j] = max(dp[i][j], dp[i - 1][j], temp_score + dp[i - 1][j - k])
+            # print(dp[i][j])
 
 print(dp[M][K])
+# print(dp)
