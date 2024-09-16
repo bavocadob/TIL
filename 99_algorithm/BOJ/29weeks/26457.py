16e6985e2c01387f4254e3c40f354a0f37d4384f
bavocadob <1ksy3629@gmail.com>
2024-09-16 23:03:40 +0900

    Feat : 0916 백준

--- 99_algorithm/BOJ/29weeks/26457.py
+++ 99_algorithm/BOJ/29weeks/26457.py
@@ -12,16 +12,17 @@ for _ in range(S):
 
 students.sort()
 
-dp = [[0] * (N + 1) for _ in range(S + 1)]
+dp = [[0] * N for _ in range(S + 1)]
 
 ans = 0
 
-for i in range(1, S + 1):
+for i in range(S):
 
-    ans = max(ans, dp[i - 1][N - 1] + students[i - 1][1] - students[i - 1][0])
+    if i >= N:
+        ans = max(ans, dp[i - 1][N - 1] + students[i][1] - students[i][0])
 
-    dp[i][1] = max(dp[i - 1][1], students[i - 1][0] + students[i - 1][1])
-    for j in range(2, N + 1):
-        dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1] + students[i - 1][1])
+    dp[i][1] = max(dp[i - 1][1], students[i][0] + students[i][1])
+    for j in range(2, min(i + 1, N)):
+        dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1] + students[i][1])
 
 print(ans)
