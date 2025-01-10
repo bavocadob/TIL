ddbd2d8b5af9bdbed4e03d2af6e16ba842d3a358
bavocadob <1ksy3629@gmail.com>
2025-01-10 23:02:37 +0900

    Feat : 25.01.10 백준

--- 99_algorithm/BOJ/42weeks/17395.py
+++ 99_algorithm/BOJ/42weeks/17395.py
@@ -1,15 +1,25 @@
 def solve():
     n, x = map(int, input().split())
+
+    cnt = bin(n).count('1')
+    if x > cnt:
+        print(-1)
+        return
+
+    if x == 1:
+        print(0)
+        return
+
     ans = []
     for i in range(60, -1, -1):
         if n & (1 << i):
             n ^= (1 << i)
             ans.append(n)
             x -= 1
-            if x == 1:
-                print(*ans, 0)
-                return
-    print(-1)
+            if x <= 1:
+                break
+
+    print(*ans, 0)
 
 
 solve()
