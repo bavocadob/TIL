e343b9b111c59e67bc3aa2cf83d33898b166c903
bavocadob <1ksy3629@gmail.com>
2025-01-24 22:52:47 +0900

    Feat : 25.01.24 백준

---
+++ 99_algorithm/BOJ/43weeks/11211.py
@@ -0,0 +1,24 @@
+def solve():
+    D, N = map(float, input().split())
+    N = int(N)
+    lo, hi = 0.0, 1.0
+    total_sum = 0
+
+    steps = list(map(int, input().split()))
+    for i in range(1, N + 1):
+        step = steps[i - 1]
+        total_sum += step
+        temp_lo = total_sum - i * D
+        temp_hi = temp_lo + 1
+
+        lo = max(lo, temp_lo)
+        hi = min(hi, temp_hi)
+
+        if lo > hi:
+            print("impossible")
+            return
+
+    print("possible")
+
+
+solve()
